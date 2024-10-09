7151c15cc3604131f171292c5130843dc1f2d881
upikoth <upikoth@gmail.com>
2024-10-09 20:50:47 +0300

    feat: добавил валидацию на имя проекта

--- internal/services/newproject/create_new_project_name.go
+++ internal/services/newproject/create_new_project_name.go
@@ -3,19 +3,42 @@ package newproject
 import (
 	"context"
 	"fmt"
+	"regexp"
 )
 
 func (p *Service) CreateNewProjectName(ctx context.Context) error {
 	fmt.Println("Введите название нового приложения. Латиница, строчные буквы, разделитель - дефис.")
 
-	name, err := p.repositories.ConsoleInput.GetString(ctx)
+	name := ""
+	isNameValid := false
 
-	if err != nil {
-		return err
+	for !isNameValid {
+		var err error
+		name, err = p.repositories.ConsoleInput.GetString(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		if checkIsProjectNameValid(name) {
+			isNameValid = true
+		} else {
+			fmt.Println("Введенное имя не соответствует формату. Латиница, строчные буквы, разделитель - дефис.")
+		}
 	}
 
 	p.newProject.SetName(name)
 	p.logger.Info(fmt.Sprintf("Название нового приложения - %s", name))
 
 	return nil
+}
+
+func checkIsProjectNameValid(projectName string) bool {
+	isMatch, err := regexp.MatchString("^[a-z][a-z0-9-]*$", projectName)
+
+	if err != nil {
+		return false
+	}
+
+	return isMatch
 }
